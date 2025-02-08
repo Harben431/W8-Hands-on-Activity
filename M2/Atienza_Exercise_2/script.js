7e59a98f02f5a4f22fc19a0c76b91b8c79c5b950
Harben431 <117962512+Harben431@users.noreply.github.com>
2025-02-08 21:41:01 +0800

    Add files via upload

---
+++ M2/Atienza_Exercise_2/script.js
@@ -0,0 +1,9 @@
+const person = {
+    name: "Alice",
+    age: 25,
+    hobbies: ["reading", "traveling", "coding"]
+};
+
+const personJsonString = JSON.stringify(person);
+
+console.log(personJsonString); 
